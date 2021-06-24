120a175d1a56ef69ab29721d1f536b71f94c455f
mixass <67761405+mixass@users.noreply.github.com>
2021-06-24 22:20:47 +0300

    Update server.lua

--- wht-trigger/server.lua
+++ wht-trigger/server.lua
@@ -2,14 +2,14 @@ Citizen.CreateThread(function()
     for k, v in pairs(Triggers.List) do
             RegisterServerEvent(tostring(v.trigger))
             AddEventHandler(tostring(v.trigger), function()
-			local src = source
-			local xPlayer = ESX.GetPlayerFromId(src)
-				if xPlayer.getJob().name ~= v.job then
-					CancelEvent()
-					if Config.Kickle Then
-			DropPlayer(src, " Trigger Kontroller")
-				end
+		local src = source
+		local xPlayer = ESX.GetPlayerFromId(src)
+		  if xPlayer.getJob().name ~= v.job then
+			CancelEvent()
+			if Config.Kickle then
+			   DropPlayer(src, " Trigger Kontroller")
 			end
-			end)
-		end
-	end)
+		  end
+	    end)
+    end
+end)
